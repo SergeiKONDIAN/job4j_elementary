d608c9dbe658fac2bcf6963eb820cb1bc6861257
Sergei <kondjansereg@mail.ru>
2021-11-16 20:58:02 +0700

    Revert "1.2. Входные данные в тесте [#297750]"
    This reverts commit ec1d0c9373283f90fb49982446e9a339ec4b9fd5.

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -21,8 +21,19 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X1Then3() {
+    public void whenA1B1C1X1Then4() {
         int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 4;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA0B1C1X1Then3() {
+        int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
@@ -32,23 +43,12 @@ public class X2Test {
     }
 
     @Test
-    public void whenA0B1C1X1Then2() {
-        int a = 0;
-        int b = 1;
-        int c = 1;
-        int x = 1;
-        int expected = 2;
-        int rsl = X2.calc(a, b, c, x);
-        Assert.assertEquals(expected, rsl);
-    }
-
-    @Test
-    public void whenA1B1C0X1Then2() {
+    public void whenA1B1C0X1Then3() {
         int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
-        int expected = 2;
+        int expected = 3;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
