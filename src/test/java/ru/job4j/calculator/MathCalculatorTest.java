237838126778fc286b269b78e8f4d9d274d745a8
Sergei <kondjansereg@mail.ru>
2021-11-17 18:21:01 +0700

    1.1. Что такое import [#504773 #229176]

--- src/test/java/ru/job4j/calculator/MathCalculatorTest.java
+++ src/test/java/ru/job4j/calculator/MathCalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class MathCalculatorTest {
 
     @Test
-    public void whenSum1AndMultiply2Then6() {
+    public void whenSum1AndMultiply2Then5() {
         double first = 1;
         double second = 2;
         double expected = 5;
@@ -16,20 +16,20 @@ public class MathCalculatorTest {
     }
 
     @Test
-    public void whenDifference1AndDivide2Then0p5() {
+    public void whenDifference1AndDivide2Then0dot5() {
         double first = 1;
         double second = 2;
-        double expected = 1 - 2 + (double) 1 / 2;
+        double expected = -0.5;
         double out = MathCalculator.differenceAndDivide(first, second);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
     }
 
     @Test
-    public void when1Sum2AllThen4p5() {
+    public void when1Sum2AllThen4dot5() {
         double first = 1;
         double second = 2;
-        double expected = 3 - 1 + 2 + (double) 1 / 2;
+        double expected = 4.5;
         double out = MathCalculator.sumAll(first, second);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
